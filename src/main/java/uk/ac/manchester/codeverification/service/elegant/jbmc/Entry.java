26fea4830335b58b1be8d2d63ed2cca529605dd8
Orion Papadakis <orion.papadakis@postgrad.manchester.ac.uk>
2022-11-08 15:15:34 +0200

    [refactor] Move id to Entry

--- src/main/java/uk/ac/manchester/codeverification/service/elegant/jbmc/Entry.java
+++ src/main/java/uk/ac/manchester/codeverification/service/elegant/jbmc/Entry.java
@@ -3,14 +3,30 @@ package uk.ac.manchester.codeverification.service.elegant.jbmc;
 import uk.ac.manchester.codeverification.service.elegant.input.Klass;
 
 public class Entry {
+    /**
+     * The id of a code verification request.
+     */
+    private long id;
     private Klass   klass;
     private String  output;
     private int     exitCode;
 
     public Entry(Klass klass, String output, int exitCode) {
+        this.id = -99;
         this.klass = klass;
         this.output = output;
         this.exitCode = exitCode;
+    }
+
+    public Entry(int id, Klass klass, String output, int exitCode) {
+        this.id = id;
+        this.klass = klass;
+        this.output = output;
+        this.exitCode = exitCode;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public void setKlass(Klass klass) {
@@ -23,6 +39,10 @@ public class Entry {
 
     public void setExitCode(int exitCode) {
         this.exitCode = exitCode;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public Klass getKlass() {
