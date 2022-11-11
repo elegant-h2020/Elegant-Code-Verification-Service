d5a76128d7632ac431fa1df34599d0a85f03a0bc
Orion Papadakis <orion.papadakis@postgrad.manchester.ac.uk>
2022-11-11 14:17:10 +0200

    [feat] Add 2 methods in the test code

--- test-cases/my/petty/examples/Simple.java
+++ test-cases/my/petty/examples/Simple.java
@@ -2,10 +2,20 @@ package my.petty.examples;
 
 public class Simple {
 
+    public static void foo() {
+        System.out.println("Hello from foo.");
+    }
+
+    public static boolean foo(String m) {
+        return m.equals("AB");  // This might create Null Pointer Exception
+    }
+
     public static void main(String[] args) {
+        System.out.println("Hello");
     	String s = new String("Abc");
         String u = "bc";
         assert(!s.contains(u)); // This assert failure should be found by JBMC!
+
     }
 
 }
