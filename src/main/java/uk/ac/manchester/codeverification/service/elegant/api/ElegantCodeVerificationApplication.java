4b4e2f522171d11a6df0c5125340f253146695d5
Orion Papadakis <orion.papadakis@postgrad.manchester.ac.uk>
2022-12-07 17:28:47 +0200

    [feat] Import MultiPart package of Jersey

--- src/main/java/uk/ac/manchester/codeverification/service/elegant/api/ElegantCodeVerificationApplication.java
+++ src/main/java/uk/ac/manchester/codeverification/service/elegant/api/ElegantCodeVerificationApplication.java
@@ -2,8 +2,12 @@ package uk.ac.manchester.codeverification.service.elegant.api;
 
 import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 @ApplicationPath("/api")
 public class ElegantCodeVerificationApplication extends ResourceConfig {
 
+    public ElegantCodeVerificationApplication() {
+        super(ElegantCodeVerificationService.class, MultiPartFeature.class);
+    }
 }
